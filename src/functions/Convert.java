253c9c4f93d973baa1addecd1883a95483c62d35
Tassio Ferenzini Martins Sirqueira <tassio@tassio.eti.br>
2017-10-17 17:46:29 -0200

    Corrections
    Corrections

--- src/functions/Convert.java
+++ src/functions/Convert.java
@@ -54,12 +54,69 @@ public class Convert {
                             source = "2";
                         }
                         city = TbCityDAO.getInstance().get(data[1]);
-                        beneficiaries = TbBeneficiariesDAO.getInstance().get(data[7]);
+                        beneficiaries = TbBeneficiariesDAO.getInstance().search(data[7],data[8].toUpperCase());
 
-                        //SQL = "INSERT INTO `DB_ECA`.`tb_payments` (`tb_city_id_city`,`tb_functions_id_function`,`tb_subfunctions_id_subfunction`,`tb_program_id_program`,`tb_action_id_action`,`tb_beneficiaries_id_beneficiaries`,`tb_source_id_source`,`tb_files_id_file`,`db_value`)" + "VALUES" + "(" + "(SELECT id_city FROM DB_ECA.tb_city where str_cod_siafi_city=" + data[1] + ")," + "1," + "1," + "1," + "1," + "(SELECT id_beneficiaries FROM DB_ECA.tb_beneficiaries where str_nis=" + data[7] + ")," + source + "," + "1," + data[10].replaceAll(",", "") + ");";
                         SQL = "INSERT INTO `DB_ECA`.`tb_payments` (`tb_city_id_city`,`tb_functions_id_function`,`tb_subfunctions_id_subfunction`,`tb_program_id_program`,`tb_action_id_action`,`tb_beneficiaries_id_beneficiaries`,`tb_source_id_source`,`tb_files_id_file`,`db_value`)" + "VALUES" + "(" + city.getIdCity() + "," + "1," + "1," + "1," + "1," + beneficiaries + "," + source + "," + "1," + data[10].replaceAll(",", "") + ");";
 
                         StrW.write(SQL);
+                    }
+                    total++;
+                }
+                StrW.write("UNLOCK TABLES;");
+                StrW.close();
+            } catch (FileNotFoundException e) {
+                logger.error("Unexpected error", e);
+            } catch (IOException e) {
+                logger.error("Unexpected error", e);
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                        logger.trace("Ended Method Convert");
+                    } catch (IOException e) {
+                        logger.error("Unexpected error", e);
+                    }
+                }
+            }
+        }
+    }
+
+    public void convertPBF_SQL_Beneciaries() {
+        logger.trace("Starting Method Convert");
+        File fInput = new File("/Users/tassio/NetBeansProjects/ECA-Importer/CSV/Pagamentos");
+        File fOutput = new File("/Users/tassio/NetBeansProjects/ECA-Importer/CSV_Converter/Pagamentos");
+        BufferedReader br = null;
+        String line = "";
+        String csvDivisor = "\t";
+        File[] filesCSV = fInput.listFiles();
+        String SQL;
+        TbBeneficiaries beneficiaries;
+
+        for (int j = 0; j < filesCSV.length; j++) {
+            File fileCSV = filesCSV[j];
+            long total = 0;
+            try {
+                OutputStreamWriter StrW = new OutputStreamWriter(new FileOutputStream(fOutput + "/" + fileCSV.getName().replaceAll("csv", "sql")), "ISO-8859-1");
+                br = new BufferedReader(new InputStreamReader(new FileInputStream(fInput + "/" + fileCSV.getName()), "ISO-8859-1"));
+                System.out.println(fileCSV.getName());
+                StrW.write("Use DB_ECA;");
+                StrW.write("LOCK TABLES `tb_beneficiaries` WRITE;");
+                while ((line = br.readLine()) != null) {
+                    String[] data = line.split(csvDivisor);
+
+                    //verificação por partes
+                    if (total != 0 && total >= 9000000 && total <= 10000000) {
+
+                        beneficiaries = TbBeneficiariesDAO.getInstance().search(data[7], data[8].toUpperCase());
+                        if (beneficiaries == null) {
+                            SQL = "INSERT INTO `DB_ECA`.`tb_beneficiaries` (`str_nis`,`str_name_person`) VALUES (" + data[7] + ",'" + data[8].toUpperCase() + "');";
+                            StrW.write(SQL);
+                        }
+
+                        if (total % 10000 == 0) {
+                            System.out.println("Lines = " + total);
+                            System.gc();
+                        }
                     }
                     total++;
                 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -202,12 +202,29 @@ public class Main {
             }
         };
 
-        Thread ConvertSQL = new Thread() {
+        Thread ConvertSQLPayment = new Thread() {
             @Override
             public void run() {
                 long StartTime = System.currentTimeMillis();
                 Convert csql = new Convert();
                 csql.convertPBF_SQL_Payments();
+                System.gc();
+                long EndTime = System.currentTimeMillis();
+                long totalms = ((EndTime - StartTime));
+                long totalsec = (totalms / 1000) % 60;
+                long totalmin = (totalms / 60000) % 60;
+                long totalh = (totalms / 3600000);
+                System.out.println("Files Imports - Total time ('HHH':'mm':'ss'.'SSS'): " + String.format("%03d:%02d:%02d.%03d", totalh, totalmin, totalsec, totalms));
+                System.exit(0);
+            }
+        };
+
+        Thread ConvertSQLBenficiaries = new Thread() {
+            @Override
+            public void run() {
+                long StartTime = System.currentTimeMillis();
+                Convert csql = new Convert();
+                csql.convertPBF_SQL_Beneciaries();
                 System.gc();
                 long EndTime = System.currentTimeMillis();
                 long totalms = ((EndTime - StartTime));
@@ -237,7 +254,7 @@ public class Main {
         };
 
         //Imports for ports the datas
-        impPBFBeneficiaries.start();
+        //impPBFBeneficiaries.start();
         //impPBFFiles.start();
         /*impPBFAction.start();
         impPBFCities.start();
@@ -252,7 +269,8 @@ public class Main {
         //Imports payments via SQL
         //impPBFOPT.start();
         //Geration SQL Import
-        //ConvertSQL.start();
+        //ConvertSQLPayment.start();
+        ConvertSQLBenficiaries.start();
         //Validate Data Import
         //ValidateData.start();
     }
